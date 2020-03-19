674c357234aeb1da305aaaab4c199bed7aa277b5
KROSF <rodrigosanabria22@gmail.com>
2020-03-19 18:15:36 +0100

    feat: add link on caveats to add zoxide to shell

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -9,10 +9,16 @@ class Zoxide < Formula
     sha256 "9867717453415b34e5ff71be102c750b42e64916ce10f080fc61f7a8a4731313"
   elsif OS.linux?
     url "https://github.com/ajeetdsouza/zoxide/releases/download/v0.2.1/zoxide-x86_64-unknown-linux-gnu"
+    sha256 "75c9e39aba583b91ec50ede3a6cba45748eec58ceb8fe09fccba467487843a0a"
   end
 
   def install
     system "mv #{buildpath}/*zoxide* #{buildpath}/zoxide"
     bin.install "zoxide"
   end
+
+  def caveats; <<~EOS
+    Adding zoxide to your shell
+    see https://github.com/ajeetdsouza/zoxide#step-2-adding-zoxide-to-your-shell
+  EOS
 end
