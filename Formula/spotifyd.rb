a2344f75ded15371dfda13d19655b1308ece90fc
KROSF <rodrigosanabria22@gmail.com>
2020-04-27 01:37:26 +0200

    feat: add plist spotifyd

--- Formula/spotifyd.rb
+++ Formula/spotifyd.rb
@@ -15,6 +15,33 @@ class Spotifyd < Formula
   depends_on "dbus"
   depends_on "portaudio"
 
+  plist_options :manual => "spotifyd"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>/usr/local/bin/spotifyd</string>
+            <string>--config-path=/users/#{ENV.USER}/.config/spotifyd/spotifyd.conf</string>
+            <string>--no-daemon</string>
+          </array>
+          <key>UserName</key>
+          <string>#{ENV.USER}</string>
+          <key>KeepAlive</key>
+          <true/>
+          <key>ThrottleInterval</key>
+          <integer>30</integer>
+        </dict>
+      </plist>
+    EOS
+  end
+
   def install
     bin.install "spotifyd"
   end
