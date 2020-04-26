3778743e5502af70cb6542c0f401e95d90a032ee
KROSF <rodrigosanabria22@gmail.com>
2020-04-27 01:03:06 +0200

    feat: add spotifyd

---
+++ Formula/spotifyd.rb
@@ -0,0 +1,22 @@
+class Spotifyd < Formula
+  desc "A cd command that learns your habits"
+  homepage "https://github.com/Spotifyd/spotifyd"
+  version "0.2.24"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/Spotifyd/spotifyd/releases/download/v#{version}/spotifyd-macos-full.tar.gz"
+    sha256 "93b0d8e5d5ea4549fbdf5ec468a790ca28609cff876f2d09251e46a9ae323afa"
+  elsif OS.linux?
+    url "https://github.com/Spotifyd/spotifyd/releases/download/v#{version}/spotifyd-linux-full.tar.gz"
+    sha256 "a3d01b7d74e49fa5c67946b9648f5b9c44de8290196b1858667503d8d53aa9e5"
+  end
+
+  def install
+    bin.install "spotifyd"
+  end
+
+  test do
+    system "#{bin}/spotifyd", "--version"
+  end
+end
