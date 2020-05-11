0b1fd07b2812fc415399ae155e688c2588694faa
KROSF <rodrigosanabria22@gmail.com>
2020-05-11 12:51:00 +0200

    feat: update zoxide to v0.4.0

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -1,15 +1,15 @@
 class Zoxide < Formula
   desc "A cd command that learns your habits"
   homepage "https://github.com/ajeetdsouza/zoxide"
-  version "0.3.1"
+  version "0.4.0"
   bottle :unneeded
 
   if OS.mac?
     url "https://github.com/ajeetdsouza/zoxide/releases/download/v#{version}/zoxide-x86_64-apple-darwin"
-    sha256 "8fb5df3b8114d61ca83e78cdaa20dac787185d9514237a06b24bfcff8c47a7fa"
+    sha256 "3fa9a5b1fd1b475e58146b3b926bf378c3ac46e29a3a2a47dcf66049dbf37dfc"
   elsif OS.linux?
     url "https://github.com/ajeetdsouza/zoxide/releases/download/v#{version}/zoxide-x86_64-unknown-linux-gnu"
-    sha256 "2e5fca4c457ce7eb97b8fe74850aff08518774ad48596c9ac7fa9525b5429a6c"
+    sha256 "7cf5a3e3ee001042de0fd2fc2fb0d316c0d77eca67df9525de4f8a44ea8d4251"
   end
 
   def install
